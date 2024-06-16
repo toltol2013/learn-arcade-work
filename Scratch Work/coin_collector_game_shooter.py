1f4ae5a0f632ec2be00f4c146a2eaa337e3eab15
batecsw <batecsw@hotmail.com>
2024-06-16 07:57:52 +0100

    Update scratch work files for lesson 8

---
+++ Scratch Work/coin_collector_game_shooter.py
@@ -0,0 +1,166 @@
+"""
+This adapts the previous static coin collector game by the player shooting bullets to destroy the coins
+The main changes to the code are as follows:
+1. we create a 3rd sprite list to handle the bullets
+2. the mouse movement function is changed so the player can only move left/right
+3. a new mouse click function creates a bullet when the mouse is clicked, positions it, and adds it to the
+   bullet sprite list
+4. the window class update function has a new loop added - this loops through the bullet list and checks if
+   the bullet has hit a coin, if so the bullet and coin are removed and the score increased. Also, if the
+   bullet has moved off the top of the screen the bullet is removed.
+"""
+
+
+import random
+import arcade
+
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.3
+SPRITE_SCALING_LASER = 0.8
+COIN_COUNT = 220
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+
+BULLET_SPEED = 5
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self):
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites and Bullets Demo")
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+        self.coin_list = None
+        self.bullet_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+        self.score = 0
+
+        # Don't show the mouse cursor
+        self.set_mouse_visible(False)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+        self.laser_sound = arcade.load_sound("laser.wav")
+        self.boom_sound = arcade.load_sound(":resources:sounds/explosion1.wav")
+
+    def setup(self):
+
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+
+        # Set up the score
+        self.score = 0
+
+        # Set up the player
+        # Image from builtin resources
+        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/male_person/malePerson_idle.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 70
+        self.player_list.append(self.player_sprite)
+
+        # Create the coins
+        for i in range(COIN_COUNT):
+
+            # Create the coin instance
+            # Coin image from builtin resources
+            coin = arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
+
+            # Position the coin
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(120, SCREEN_HEIGHT)
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites.
+        self.coin_list.draw()
+        self.bullet_list.draw()
+        self.player_list.draw()
+
+        # Render the text
+        arcade.draw_text(f"Score: {self.score}", 10, 20, arcade.color.WHITE, 14)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """
+        Called whenever the mouse moves.
+        """
+        self.player_sprite.center_x = x
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called whenever the mouse button is clicked.
+        """
+
+        # Create a bullet
+        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
+        arcade.play_sound(self.laser_sound)
+
+        # The image points to the right, and we want it to point up. So
+        # rotate it.
+        bullet.angle = 90
+
+        # Position the bullet
+        bullet.center_x = self.player_sprite.center_x
+        bullet.bottom = self.player_sprite.top
+        bullet.change_y = BULLET_SPEED
+
+        # Add the bullet to the appropriate lists
+        self.bullet_list.append(bullet)
+
+    def update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites
+        self.coin_list.update()
+        self.bullet_list.update()
+
+        # Loop through each bullet
+        for bullet in self.bullet_list:
+
+            # Check this bullet to see if it hit a coin
+            hit_list = arcade.check_for_collision_with_list(bullet, self.coin_list)
+
+            # If it did, get rid of the bullet
+            if len(hit_list) > 0:
+                bullet.remove_from_sprite_lists()
+
+            # For every coin we hit, add to the score and remove the coin
+            for coin in hit_list:
+                coin.remove_from_sprite_lists()
+                self.score += 1
+                arcade.play_sound(self.boom_sound)
+
+            # If the bullet flies off-screen, remove it.
+            if bullet.bottom > SCREEN_HEIGHT:
+                bullet.remove_from_sprite_lists()
+
+
+def main():
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
