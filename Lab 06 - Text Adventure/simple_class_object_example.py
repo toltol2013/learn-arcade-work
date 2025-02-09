010c1d75312eff983810e71ca66ed437cf096176
toltol2013 <anatoletoklu@gmail.com>
2025-02-09 13:04:24 +0100

    classes

--- Lab 06 - Text Adventure/simple_class_object_example.py
+++ Lab 06 - Text Adventure/simple_class_object_example.py
@@ -1,14 +1,16 @@
 class Dog():
-    def __init__(self, dog_name):
+    def __init__(self, dog_name, dog_color):
         """ Constructor """
         self.name = dog_name
+        self.color = dog_color
         print("A new dog is born!")
 
 
 def main():
     # This creates the dog object
-    my_dog = Dog("Bouncer")
+    my_dog = Dog("Bouncer", "black")
+    my_dog.color = "black"
     print("The dog's name is: ", my_dog.name)
-
+    print("The dog's colour is", my_dog.color)
 
 main()
