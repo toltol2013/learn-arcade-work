a3a4ab4bf02b8391fcdb668b118180ec34bfd229
toltol2013 <anatoletoklu@gmail.com>
2025-03-02 12:07:44 +0100

    Update my_house.py

--- Lab 06 - Text Adventure/my_house.py
+++ Lab 06 - Text Adventure/my_house.py
@@ -16,21 +16,24 @@ def main():
 
 
     room_list = []
-    names = ["Hall", "Bathroom", "Living room", "Bedroom", "Library", "Kitchen"]
+    names = ["Hall", "Bathroom", "Living room", "Bedroom", "Library", "Kitchen", "bedroom2", "bathroom2", "bedroom3"]
     descriptions = ["You are in the Hall. At your right you have a shoe cabinet to put your shoes inside. There is also a little window. There are doors to the north and to the east.",
         "You are in the Bathroom. at your left you have a toilet and in front of you a shower. There are doors to the east and to the west.",
         "you are in the Living room. In front of you you have a sofa and at you right a TV. There are doors to the west and to the north.",
         "You are in the Bedroom. In front of you there is a big bed and on your right there is a wardrobe. There are doors om the south and on the west.",
         "You are in the Library. There are a lot of books and there is a table with some chairs. There are doors on the east and on the west.",
-        "You are in the kitchen. There si a big table with some chairs. There is also a fridge and a cooker."
+        "You are in the kitchen. There si a big table with some chairs. There is also a fridge and a cooker. There are doors to the sout and to the east.",
+        "You are in the second bedroom. In front of you there is a bed and an armchair. At your left you have a window with curtains. There is a door to the east.",
+        "You are in the second bathroom. in front of you there is a little window without curtains, there is a toilet and a bath. There is also a shower. There are doors to the east and to the west.",
+        "You are in the third bedroom. It's a really big room. At your right there is a big shelf. In front of you there is a really big bed and at your right a big window with yellow curtains. There is a door at the west."
     ]
 
-    norths = [5, None, 3, None, None, None]
-    easts = [1, 2, None, None, 3, 4]
-    souths = [None, None, None, 2, None, 0]
-    wests = [None, 0, 1, 4, 5, None]
-    ups = [None, None, None, None, None, None]
-    downs = [None, None, None, None, None, None]
+    norths = [5, None, 3, None, None, None, None, None, None]
+    easts = [1, 2, None, None, 3, 4, None, None, None]
+    souths = [None, None, None, 2, None, 0, None, None, None]
+    wests = [None, 0, 1, 4, 5, None, None, None, None]
+    ups = [6, None, None, None, None, None, None, None, None]
+    downs = [None, None, None, None, None, None, 0, None, None]
 
     for i in range(len(names)):
 
@@ -76,7 +79,7 @@ def main():
                 current_room = new_room
 
         elif move == "w":
-            new_room = room_list[current_room].south
+            new_room = room_list[current_room].west
 
             if new_room is None:
                 print("Sorry, you can't go that way")
